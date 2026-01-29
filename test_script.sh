7e9b99eb45d7c0d8bad2d14e26da9062b25972b6
James Standish <jstandish@hpc-ext-1.cam.uchc.edu>
2026-01-29 09:55:14 -0500

    main: second conflicting edit

--- test_script.sh
+++ test_script.sh
@@ -3,4 +3,5 @@ echo "Hello Git!!!! Glad youre here!"
 echo "I'm adding a second line!!!!"
 
 echo "Hello from main, this is gonna be my first edit lmao"
+echo "Hello from main, this is gonna be my 2nd edit, cool? cool."
 
