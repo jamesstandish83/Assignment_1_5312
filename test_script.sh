46530f29d7ccf6e1f2022d875f960022c9b2851f
James Standish <jstandish@hpc-ext-1.cam.uchc.edu>
2026-01-29 09:50:50 -0500

    newbranch: second edit

--- test_script.sh
+++ test_script.sh
@@ -3,4 +3,5 @@ echo "Hello Git!!!! Glad youre here!"
 echo "I'm adding a second line!!!!"
 
 echo "Hello from newbranch, this is gonna be my first edit lol"
+echo "Hello from newbranch, this is gonna be my 2nd edit, ya dig?"
 
